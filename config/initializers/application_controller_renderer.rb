bcd684925a34f01e78f89cb8b01b5033892d2924
akiho-natsuhara <natsu.050711@gmail.com>
2022-03-12 17:34:29 +0900

    first commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'eef0f43aae0d2e31191864f515fe8c28d0b6a109c988b4db36130581ee4e652a2fb62fd5bc71e94274bbacb2fce255c56e080ec5795b6a475d026acbd50eeeef'
