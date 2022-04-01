5064585db9227255e5bff5bb111347ce0302c612
akiho-natsuhara <natsu.050711@gmail.com>
2022-04-01 15:13:48 +0900

    seed導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: '田中大輔', profile:'料理始めました!', email:'tanaka@sample.com', password:'tanaka1234')
+User.create!(name: '鈴木太郎', profile:'料理が好きです!', email:'suzuki@sample.com', password:'suzuki1234')
+
+Recipe.create!(user_id: User.first, title: 'タラバガニのパスタ', body: 'タラバガニ、エビ、プチトマトをニンニクと鷹の爪で作成したオイルベースのパスタです。')
+Recipe.create!(user_id: User.second, title: 'ローストビーフ', body: '2時間かけてじっくり焼き上げました。')
+
+Relationship.create!(follower_id: User.first, followed_id: User.second)
+Relationship.create!(follower_id: User.second, followed_id: User.first)
+
+Comment.create!(user_id: User.second, recipe_id: Recipe.first, comment: 'とても美味しそうです!')
+Comment.create!(user_id: User.first, recipe_id: Recipe.second, comment: 'いつも参考にしています。')
+
+Favorite.create!(user_id: User.first, recipe_id: Recipe.second)
+Favorite.create!(user_id: User.second, recipe_id: Recipe.first)
